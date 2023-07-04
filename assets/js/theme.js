c1f591691a4dde29bf5deaaa7f15e5b15fa972b8
Dmitrijs Osipovs <dmitrijsosipov@gmail.com>
2023-07-04 19:15:50 +0300

    burger interactive

---
+++ assets/js/theme.js
@@ -0,0 +1,6 @@
+const handleMenu = () => {
+    document.body.classList.toggle("lock-scroll");
+    document.getElementById("openImg").classList.toggle("opened");
+    document.getElementById("closeImg").classList.toggle("opened");
+    document.getElementById("mobileMenu").classList.toggle("toggleMenu")
+}
